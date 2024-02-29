2f683eb8902fec07a303bbe85e251e5b49cfa7c4
hgshkt <ilendzeier.artem@lll.kpi.ua>
2024-02-29 17:25:05 +0200

    connect main activity and splash fragment to view model

--- app/src/main/java/com/hgshkt/todolistfirebase/MainActivity.kt
+++ app/src/main/java/com/hgshkt/todolistfirebase/MainActivity.kt
@@ -1,11 +1,62 @@
 package com.hgshkt.todolistfirebase
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.api.ApiException
+import com.hgshkt.todolistfirebase.view.viewModel.MainViewModel
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnAuthActivity {
+
+    private val signInRequestCode = 1
+    private var viewModel: MainViewModel? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel!!.uiState.observeForever { state ->
+            when (state) {
+                is MainViewModel.UIState.LoginScreen -> updateUiToLoginScreen()
+                is MainViewModel.UIState.ToDoListScreen -> updateUiToListScreen()
+                is MainViewModel.UIState.CreateTaskScreen -> updateUiToCreateTaskScreen()
+            }
+        }
     }
+
+    private fun updateUiToCreateTaskScreen() {
+        TODO("Not yet implemented")
+    }
+
+    private fun updateUiToListScreen() {
+        TODO("Not yet implemented")
+    }
+
+    private fun updateUiToLoginScreen() {
+        TODO("Not yet implemented")
+    }
+
+    override fun launch(intent: Intent) {
+        startActivityForResult(intent, signInRequestCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            signInRequestCode -> viewModel!!.signInWithCredential(
+                data = data!!,
+                afterSignIn = { updateUiToListScreen() },
+                errorHandling = { e -> signInError(e) }
+            )
+        }
+    }
+
+    private fun signInError(e: ApiException) {
+        Toast.makeText(this, "$e", Toast.LENGTH_LONG).show()
+    }
+}
+
+interface OnAuthActivity {
+    fun launch(intent: Intent)
 }
--- app/src/main/java/com/hgshkt/todolistfirebase/view/fragments/SplashFragment.kt
+++ app/src/main/java/com/hgshkt/todolistfirebase/view/fragments/SplashFragment.kt
@@ -1,6 +1,42 @@
 package com.hgshkt.todolistfirebase.view.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.common.SignInButton
+import com.hgshkt.todolistfirebase.OnAuthActivity
+import com.hgshkt.todolistfirebase.R
+import com.hgshkt.todolistfirebase.view.viewModel.MainViewModel
 
-class SplashFragment: Fragment() {
+class SplashFragment : Fragment() {
+    private lateinit var signInButton: SignInButton
+    private var viewModel: MainViewModel? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.splash_login_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+
+        signInButton.setOnClickListener {
+            val activity = requireActivity() as OnAuthActivity
+
+            viewModel!!.signInButtonClick(requireContext()) { intent ->
+                activity.launch(intent)
+            }
+        }
+    }
+
+    private fun init(view: View) {
+        signInButton = view.findViewById(R.id.signInButton)
+    }
 }
