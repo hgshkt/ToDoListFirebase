85d6edf1c33e87f4a4ce40af3662916180db9005
hgshkt <ilendzeier.artem@lll.kpi.ua>
2024-03-01 11:05:16 +0200

    add task list fragment

---
+++ app/src/main/java/com/hgshkt/todolistfirebase/view/fragments/list/TaskListFragment.kt
@@ -0,0 +1,32 @@
+package com.hgshkt.todolistfirebase.view.fragments.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.hgshkt.todolistfirebase.R
+
+class TaskListFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.list_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+    }
+
+    private fun init(view: View) {
+        recyclerView = view.findViewById(R.id.taskListRecyclerView)
+    }
+}
--- app/src/main/java/com/hgshkt/todolistfirebase/view/fragments/splash/SplashFragment.kt
+++ app/src/main/java/com/hgshkt/todolistfirebase/view/fragments/splash/SplashFragment.kt
@@ -1,4 +1,4 @@
-package com.hgshkt.todolistfirebase.view.fragments
+package com.hgshkt.todolistfirebase.view.fragments.splash
 
 import android.os.Bundle
 import android.view.LayoutInflater
