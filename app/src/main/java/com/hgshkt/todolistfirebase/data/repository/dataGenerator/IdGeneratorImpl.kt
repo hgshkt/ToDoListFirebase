59d35b8231d7ee166d497fad11c946de4082451d
hgshkt <ilendzeier.artem@lll.kpi.ua>
2024-03-01 14:24:39 +0200

    IdGeneratorImpl randomUUID() impl

--- app/src/main/java/com/hgshkt/todolistfirebase/data/repository/dataGenerator/IdGeneratorImpl.kt
+++ app/src/main/java/com/hgshkt/todolistfirebase/data/repository/dataGenerator/IdGeneratorImpl.kt
@@ -1,4 +1,9 @@
 package com.hgshkt.todolistfirebase.data.repository.dataGenerator
 
+import java.util.UUID
+
 class IdGeneratorImpl: IdGenerator {
+    override fun randomUUID(): String {
+        return UUID.randomUUID().toString()
+    }
 }
